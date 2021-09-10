771c37387118e9a4772758b931735fde486ad4be
120356aa <yurlovandrew@gmail.com>
2021-09-10 02:28:07 -0700

    index structure

--- src/App.svelte
+++ src/App.svelte
@@ -3,18 +3,26 @@
 </script>
 
 <main>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div class="title">
+		<h1>Andrey Yurlov</h1>
+		<h3>Full Stack Software Engineer</h3>
+	</div>
+	<div class="buttons">
+		<button class="github">
+			<img src="#" />
+			<p>Github</p>
+		</button>
+		<button class="portfolio">
+			<img src="#" />
+			<p>Portfolio</p>
+		</button>
+	</div>
 </main>
 
 <style type="text/scss">
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-
-		p {
-			color: green;
-		}
+		width: 100%;
+		height: 100%;
+		outline: 1px solid blue;
 	}
 </style>
