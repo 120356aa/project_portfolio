ac69f2ecb27317e9bfe768646021a651e7ac4b21
120356aa <yurlovandrew@gmail.com>
2021-09-10 02:14:05 -0700

    svelte init

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style type="text/scss">
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+
+		p {
+			color: green;
+		}
+	}
+</style>
